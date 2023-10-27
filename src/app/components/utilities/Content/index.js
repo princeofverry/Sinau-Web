4a2f5fa9632d9b7f258cbd4392fb93576dbf9524
princeofverry <verrykurniawan@students.undip.ac.id>
2023-10-27 19:20:44 +0700

    push gambar

--- src/app/components/utilities/Content/index.js
+++ src/app/components/utilities/Content/index.js
@@ -1,10 +1,10 @@
+import Image from "next/image";
 import React from "react";
 
 const ContentHomePage = () => {
   return (
     <>
-      {/* Menu Portofolio */}
-      <div className="bg-[#9A3B3B] text-white py-4">
+      <div className="bg-[#9A3B3B] text-white py-6">
         <div className="max-w-screen-xl mx-auto my-8">
           <p className="max-w-screen-xl text-lg font-light mx-auto text-center  text-white py-8">
             Selamat datang di dunia budaya yang memikat! Bersama kami, Anda akan
@@ -16,6 +16,7 @@ const ContentHomePage = () => {
           <ul className="flex sm:flex-row flex-col gap-16 justify-center mx-8">
             <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
               {/* Kotak dalam */}
+              <Image src="/iconBahasa.png" width={240} height={300} className="mx-auto my-4"></Image>
               <div className="bg-[#C08261] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
                 {/* Isi kotak dalam */}
                 <p className="text-white text-center">Bahasa</p>
@@ -23,13 +24,14 @@ const ContentHomePage = () => {
             </li>
             <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
               {/* Kotak dalam */}
-              
+              <Image src="/iconWarisan.png" width={200} height={230} className="mx-auto my-4"></Image>
               <div className="bg-[#C08261] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
                 {/* Isi kotak dalam */}
                 <p className="text-white text-center">Warisan</p>
               </div>
             </li>
             <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
+              <Image src="/iconCerita.png" width={160} height={400} className="mx-auto"></Image>
               {/* Kotak dalam */}
               <div className="bg-[#C08261] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
                 {/* Isi kotak dalam */}
@@ -37,6 +39,7 @@ const ContentHomePage = () => {
               </div>
             </li>
             <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
+              <Image src="/iconWayang.png" width={240} height={400} className="mx-auto"></Image>
               {/* Kotak dalam */}
               <div className="bg-[#C08261] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
                 {/* Isi kotak dalam */}
---
+++ src/app/warisan/landingWarisan.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const landingWarisan = () => {
+  return (
+    <div>landingWarisan</div>
+  )
+}
+
+export default landingWarisan
