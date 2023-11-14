3b795bf5f38974b49eaac6a6a1ad2b18e3002729
Ediw7 <ediwicoro26@gmail.com>
2023-11-14 20:50:32 +0700

    pop up wayang

--- src/app/wayang/page.js
+++ src/app/wayang/page.js
@@ -17,15 +17,15 @@ const Wayang = () => {
 
               {/* Menambahkan kotak Explore */}
               <div className="relative">
-                <div className="bg-[#9A3B3B] text-white w-32 h-12 mt-4 flex justify-center items-center rounded-md transition-all duration-300 hover:bg-[#E2C799]"style={{ fontSize: '24px' }}>
+                <div className="bg-[#E2C799] text-white w-32 h-12 mt-4 flex justify-center items-center rounded-md transition-all duration-300 hover:bg-[#9A3B3B]"style={{ fontSize: '24px' }}>
                   <span className="hover:text-[#FCF5ED]">Explore</span>
-                  <span className="ml-1">&#10132;</span>
+                  <span style={{ transform: 'rotate(90deg)' }}>&#10132;</span>
                 </div>
               </div>
             </div>
 
             <div className="hidden sm:flex">
-              <Image src="/wayang-kanan.png" width={400} height={400} alt={"..."}/>
+              <Image src="/wayang-kanan.png" width={400} height={400} />
             </div>
           </div>
         </div>
@@ -40,16 +40,13 @@ const Wayang = () => {
             d="M0,128L34.3,133.3C68.6,139,137,149,206,138.7C274.3,128,343,96,411,101.3C480,107,549,149,617,170.7C685.7,192,754,192,823,176C891.4,160,960,128,1029,101.3C1097.1,75,1166,53,1234,53.3C1302.9,53,1371,75,1406,85.3L1440,96L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"
           ></path>
         </svg>
-        <h1 className="text-4xl font-bold text-white text-center">P</h1>
+        <h1 className="text-4xl font-bold text-white text-center">Pandawa Lima</h1>
         <div className="max-w-screen-xl mx-auto my-4">
           <p className="max-w-screen-xl text-md font-light mx-auto text-center  text-white pb-8">
-            Pulau jawa adalah pusat segala hal. Kebijaksanaan kuno, budaya
-            megah, dan seni tradisional bergabung dalam harmoni yang memukau.
-            Mari kita eksplorasi pandangan penuh warna akan Jawa yang
-            menggagumkan.
+          Pandawa Lima merupakan sebutan untuk lima bersaudara pada tokoh pewayangan yang terdapat dalam kisah Mahabharata.Sesuai namanya “Pandawa Lima” terdiri dari 5 tokoh yang merupakan anak dari Pandu, seorang Raja di Hastinapura.
           </p>
           <ul className="flex sm:flex-row flex-col gap-16 justify-center mx-8">
-            <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
+          <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
               {/* Kotak dalam */}
               <Image
                 src="/iconBahasa.png"
@@ -60,7 +57,9 @@ const Wayang = () => {
               ></Image>
               <div className="bg-[#C08261] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
                 {/* Isi kotak dalam */}
-                <p className="text-white text-center">Bahasa</p>
+                <p className="text-white text-center">Yudhistira</p>
+                {/* Additional text */}
+                {/* <p className="additional-text">Yudhistira adalah anak pertama dari Kunti</p> */}
               </div>
             </li>
             <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
@@ -74,7 +73,7 @@ const Wayang = () => {
               ></Image>
               <div className="bg-[#C08261] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
                 {/* Isi kotak dalam */}
-                <p className="text-white text-center">Warisan</p>
+                <p className="text-white text-center">Bima</p>
               </div>
             </li>
             <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
@@ -88,7 +87,7 @@ const Wayang = () => {
               {/* Kotak dalam */}
               <div className="bg-[#C08261] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
                 {/* Isi kotak dalam */}
-                <p className="text-white text-center">Pojok Cerita</p>
+                <p className="text-white text-center">Arjuna</p>
               </div>
             </li>
             <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
@@ -102,7 +101,7 @@ const Wayang = () => {
               {/* Kotak dalam */}
               <div className="bg-[#C08261] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
                 {/* Isi kotak dalam */}
-                <p className="text-white text-center">Wayang</p>
+                <p className="text-white text-center">Nakula</p>
               </div>
             </li>
             <li className="bg-[#E2C799] w-80 h-72 rounded-2xl relative">
@@ -116,7 +115,7 @@ const Wayang = () => {
               {/* Kotak dalam */}
               <div className="bg-[#C08261] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
                 {/* Isi kotak dalam */}
-                <p className="text-white text-center">Wayang</p>
+                <p className="text-white text-center">Sadewa</p>
               </div>
             </li>
           </ul>
@@ -150,7 +149,6 @@ const Wayang = () => {
                 src="/iconBahasa.png"
                 width={240}
                 height={300}
-                alt={"..."}
                 className="mx-auto my-4 hover:scale-105 transition-all duration-300"
               ></Image>
               <div className="bg-[#F2ECBE] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
@@ -164,7 +162,6 @@ const Wayang = () => {
                 src="/iconWarisan.png"
                 width={200}
                 height={230}
-                alt={"..."}
                 className="mx-auto my-4 hover:scale-105 transition-all duration-300"
               ></Image>
               <div className="bg-[#F2ECBE] w-full h-16 rounded-2xl absolute bottom-0 left-0 right-0 flex justify-center items-center">
@@ -177,7 +174,6 @@ const Wayang = () => {
                 src="/iconCerita.png"
                 width={160}
                 height={400}
-                alt={"..."}
                 className="mx-auto hover:scale-105 transition-all duration-300"
               ></Image>
               {/* Kotak dalam */}
@@ -191,7 +187,6 @@ const Wayang = () => {
                 src="/iconWayang.png"
                 width={240}
                 height={400}
-                alt={"..."}
                 className="mx-auto hover:scale-105 transition-all duration-300"
               ></Image>
               {/* Kotak dalam */}
