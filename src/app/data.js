9fa319c94851093df68d27f3e15fa15a67766747
Ediw7 <ediwicoro26@gmail.com>
2023-12-08 00:19:50 +0700

    membuat quiz

---
+++ src/app/data.js
@@ -0,0 +1,36 @@
+export const quiz = {
+    totalQuestions: 5,
+    questions: [
+      {
+        id: 1,
+        question: 'Berapakah huruf dalam aksara Jawa?',
+        answers: ['18', '20', '22', '24'],
+        correctAnswer: '20',
+      },
+      {
+        id: 2,
+        question: 'Apa arti dari kata "Sugeng Rawuh" dalam bahasa Jawa?',
+        answers: ['Selamat Pagi', 'Selamat Siang', 'Selamat Malam', 'Selamat Datang'],
+        correctAnswer: 'Selamat Datang',
+      },
+      {
+        id: 3,
+        question: 'Apa arti dari kata "Mangan" dalam bahasa Jawa?',
+        answers: ['Minum', 'Makan', 'Tidur', 'Lari'],
+        correctAnswer: 'Makan',
+      },
+      {
+        id: 4,
+        question: 'Bagaimana mengucapkan "Terima kasih" dalam bahasa Jawa?',
+        answers: ['Matur Suwun', 'Sugeng Rawuh', 'Sampun', 'Rahajeng Semeng'],
+        correctAnswer: 'Matur Suwun',
+      },
+      {
+        id: 5,
+        question: 'Apa maksud dari kalimat "Pripun, pak."?',
+        answers: ['Iya, Pak', 'Tidak, Pak', 'Selamat Pagi, Pak', 'Bagaimana, Pak'],
+        correctAnswer: 'Bagaimana Pak',
+      },
+    ],
+  };
+  
